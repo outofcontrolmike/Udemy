19e907a69f4306f15450013e8e5b47552d5c27a2
outofcontrolmike <mw0415436@otc.edu>
2019-01-26 11:56:48 -0600

    Add files via upload

---
+++ Operators/KeywordsAndExpressions/src/keywordsandexpressions/KeywordsAndExpressions.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package keywordsandexpressions;
+
+/**
+ *
+ * @author Michael Wilson
+ */
+public class KeywordsAndExpressions {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double kilometers = (100 * 1.553);  // double completes java statment
+
+        int highScore = 50;
+        if (highScore == 50) {
+            System.out.println("This is an expression"); // anything in brackets
+
+        }
+        //anything after a data type is an expression
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // in the following code, write down what parts of the code are expressions
+    
+    int score = 100;
+    if(score > 99)
+    {
+        System.out.println("You go the high score");
+        score = 0;
+    }
+    
+    // score = 100;
+    // score > 99
+    // "you go the high score... string literal
+    // score = 0
+
+}
+
+}
